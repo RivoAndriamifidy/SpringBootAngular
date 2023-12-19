d9b3a2196239ff5e0ebc2924976f2be318989a16
Rivo <127342484+RivoAndriamifidy@users.noreply.github.com>
2023-12-19 12:09:43 +0300

    Add files via upload

---
+++ gestiondestock/src/test/java/com/monProjet/gestiondestock/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monProjet.gestiondestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
