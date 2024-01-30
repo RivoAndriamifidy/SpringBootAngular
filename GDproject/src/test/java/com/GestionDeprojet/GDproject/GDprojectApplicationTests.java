5f472873626ce6c24817f9068d8a44e35d8258bb
Rivo <127342484+RivoAndriamifidy@users.noreply.github.com>
2024-01-30 14:47:15 +0300

    Add files via upload

---
+++ GDproject/src/test/java/com/GestionDeprojet/GDproject/GDprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionDeprojet.GDproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GDprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
